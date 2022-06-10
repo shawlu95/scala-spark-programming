29c43488293de4d2dfbe3356903d38551f8039ee
Shaw Lu <shawlu95@gmail.com>
2022-06-09 23:07:16 -0700

    Init project

---
+++ src/main/scala/HiBailey.scala
@@ -0,0 +1,7 @@
+object HiBailey {
+  // This is a class, which requires compile
+  // Scala script does not require compile
+  def main(args: Array[String]): Unit = {
+    print("Hi Bailey")
+  }
+}
